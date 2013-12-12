ae96163499e2d5240fa04871bca49a64980db753
Yuchen Dai <silentdai@gmail.com>
2013-12-12 14:42:44 -0800

    ADD: same-tree

---
+++ src/dyc/TreeNode.java
@@ -0,0 +1,11 @@
+package dyc;
+
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
---
+++ src/newleetcode/SameTree.java
@@ -0,0 +1,9 @@
+package newleetcode;
+
+import dyc.TreeNode;
+
+public class SameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		return true;
+	}
+}
