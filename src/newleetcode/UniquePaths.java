18b733ba76eb1bf4624873e1946671d7c77fa1c9
Yuchen Dai <silentdai@gmail.com>
2013-12-28 22:43:24 -0800

    ADD: UniquePaths

---
+++ src/newleetcode/UniquePaths.java
@@ -0,0 +1,21 @@
+//http://oj.leetcode.com/problems/unique-paths/
+package newleetcode;
+
+public class UniquePaths {
+	public int uniquePaths(int m, int n) {
+		int paths[] = new int[n];
+		for (int i = 0; i < n; ++i) {
+			paths[i] = 1;
+		}
+		for (int j = 0; j < m; ++j) {
+			for (int i = 1; i < n; ++i) {
+				if (i == 0) {
+					paths[i] = 1;
+				} else {
+					paths[i] = paths[i] + paths[i - 1];
+				}
+			}
+		}
+		return paths[n - 1];
+	}
+}
