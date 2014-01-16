464d159ea7a931f18cb5c7128d0115755a38aa3e
Yuchen Dai <silentdai@gmail.com>
2014-01-16 00:40:59 -0800

    MOD: SearchRotatedSortedArray, remove the condition of interval == 2

--- src/newleetcode/SearchRotatedSortedArray.java
+++ src/newleetcode/SearchRotatedSortedArray.java
@@ -12,15 +12,15 @@ public class SearchRotatedSortedArray {
 		if (il > ir) { return -1;}
 		int imid = (il + ir) /2;
 		if (target == a[imid]) { return imid; }
-		if(il == imid) {return a[ir] == target ? ir: -1;}
+		//if(il == imid) {return a[ir] == target ? ir: -1;}
 		if (target < a[imid]) {
-			if (a[il] < a[imid]) {
+			if (a[il] <= a[imid]) {
 				return a[il] <= target ? search(a, target, il, imid-1) : searchDual(a, target, imid + 1, ir);
 			} else {
 				return searchDual(a, target, il, imid-1);
 			}
 		} else {
-			if (a[ir] > a[imid]) {
+			if (a[ir] >= a[imid]) {
 				return a[ir] >= target ? search(a, target, imid+1, ir) : searchDual(a, target, il, imid-1);
 			} else {
 				return searchDual(a, target, imid+1, ir);
